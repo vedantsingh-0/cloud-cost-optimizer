666897935ab3c6f9c351be04d7904034de73c039
Vedant <sr9453300@gmail.com>
2026-04-18 04:33:57 +0530

    Fix unused Legend import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip,
-  ResponsiveContainer, LineChart, Line, PieChart, Pie, Cell, Legend
+  ResponsiveContainer, LineChart, Line, PieChart, Pie, Cell
 } from 'recharts';
 import './App.css';
 
