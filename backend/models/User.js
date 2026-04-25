95f66f5a5b1524b1caa473bcfe468cc005e3e78e
Vedant <sr9453300@gmail.com>
2026-04-26 04:34:48 +0530

    Fix User.js remove next parameter

--- backend/models/User.js
+++ backend/models/User.js
@@ -14,14 +14,13 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
-userSchema.methods.comparePassword = function(candidate) {
+userSchema.methods.comparePassword = async function(candidate) {
   return bcrypt.compare(candidate, this.password);
 };
 
